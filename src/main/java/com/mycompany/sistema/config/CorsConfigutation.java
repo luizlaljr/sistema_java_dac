14728650b0d561f01a716ad55bad2835e0a19d41
luizlaljr <luizlaljr@gmail.com>
2021-09-26 19:01:21 -0300

    Cors adicionado e configurado

---
+++ src/main/java/com/mycompany/sistema/config/CorsConfigutation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sistema.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author luizlaljr
+ */
+public class CorsConfigutation implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
